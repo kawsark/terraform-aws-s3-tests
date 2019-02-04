a16487abaf12c9014ac511fbc98f2bfcc8974284
kawsark <kawsar@hashicorp.com>
2019-02-04 10:10:46 -0500

    fixed conflicts

--- aws.tf
+++ aws.tf
@@ -2,7 +2,14 @@ variable "aws_access_key" {}
 
 variable "aws_secret_key" {}
 
-variable "aws_session_name" {}
+variable "aws_session_name" {
+  description = "Optional session name to supply when performing AssumeRole call"
+  default = "terraform-assume-role"
+}
+
+variable "role_arn" {
+  description = "The ARN of an existing role that Terraform should Assume"
+}
 
 provider "aws" {
   region = "${var.aws_region}"
