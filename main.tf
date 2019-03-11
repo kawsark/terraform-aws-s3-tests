5237031a2d0943232a58109bd6e87c003b101358
kawsark <kawsar@hashicorp.com>
2019-03-10 21:18:18 -0400

    updated to use aws credentials from terraform variables

--- main.tf
+++ main.tf
@@ -22,8 +22,14 @@ variable "env" {
   default = "dev"
 }
 
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
 provider "aws" {
   region = "${var.aws_region}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
 }
 
 resource "aws_s3_bucket" "s3-test" {
