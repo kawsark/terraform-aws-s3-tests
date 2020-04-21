a014e7eeb92400053f8d84739669a64f9e3f7e37
Peter Leung <pleung@hashicorp.com>
2020-04-20 21:45:37 -0400

    Remove references to aws access and secret keys to support instance profiles

--- aws.tf
+++ aws.tf
@@ -14,11 +14,6 @@ variable "role_arn" {
 }
 
 provider "aws" {
-  region = var.aws_region
-
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-
   assume_role {
     role_arn     = var.role_arn
     session_name = var.aws_session_name
